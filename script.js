654e2700768d52370547f4c9a696874101d6a7a2
Fatima Rahimi <iamfatimarahimi@gmail.com>
2024-12-14 19:36:13 +0330

    [FIX] the nav bar was fixed

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-import EmblaCarousel from "embla-carousel";
+// Grab wrapper nodes
+const rootNode = document.querySelector(".embla");
+// Grab button nodes
+const prevButtonNode = rootNode.querySelector(".embla__button--prev");
+const nextButtonNode = rootNode.querySelector(".embla__button--next");
 
-const emblaNode = document.querySelector(".embla");
-const options = { loop: false };
-const emblaApi = EmblaCarousel(emblaNode, options);
+const options = { loop: true };
+// const plugins = [EmblaCarouselAutoplay()];
+const emblaApi = EmblaCarousel(rootNode, options);
+prevButtonNode.addEventListener("click", emblaApi.scrollPrev);
+nextButtonNode.addEventListener("click", emblaApi.scrollNext);
 
-console.log(emblaApi.slideNodes()); // Access API
+window.onscroll = function () {
+  const navbar = document.getElementById("mid-sec");
+  if (window.scrollY >= 100) {
+    navbar.classList.add("fixed");
+  } else {
+    navbar.classList.remove("fixed");
+  }
+};
