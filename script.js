c02b9826cc3ef409494c7bd91a73e6659eca1801
Fatima Rahimi <iamfatimarahimi@gmail.com>
2024-12-25 19:32:12 +0330

    Adding the Accordion
    Adding the Accordion css style

--- script.js
+++ script.js
@@ -12,9 +12,20 @@ nextButtonNode.addEventListener("click", emblaApi.scrollNext);
 
 window.onscroll = function () {
   const navbar = document.getElementById("mid-sec");
-  if (window.scrollY >= 100) {
+  if (window.scrollY >= 50) {
     navbar.classList.add("fixed");
   } else {
     navbar.classList.remove("fixed");
   }
 };
+// Accordion
+let head = document.getElementsByClassName("button");
+let body = document.getElementsByClassName("acoordion-body");
+for (let index of head) {
+  index.addEventListener("click", () => {
+    body[this.length].classList.add("show");
+    // console.log(this.length);
+
+    // console.log(index);
+  });
+}
